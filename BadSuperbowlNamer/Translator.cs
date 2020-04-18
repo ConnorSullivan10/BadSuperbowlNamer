13a69eb86f61b86997f5de2d3333ceb1b5e11670
Connor Sullivan <cksullvn@gmail.com>
2020-04-17 22:05:14 -0500

    finished homework assignment

--- BadSuperbowlNamer/Translator.cs
+++ BadSuperbowlNamer/Translator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BadSuperbowlNamer
+﻿namespace BadSuperbowlNamer
 {
     public class Translator
     {
@@ -12,12 +8,47 @@ namespace BadSuperbowlNamer
 
             while (number > 0)
             {
-                if (number >= 10)
+                if (number >= 1000)
+                {
+                    romanNumeral += "M";
+                    number -= 1000;
+                }
+                else if ( number >= 900)
+                {
+                    romanNumeral += "CM";
+                    number -= 900;
+                }
+                else if (number >= 500)
+                {
+                    romanNumeral += "D";
+                    number -= 500;
+                }
+                else if (number >= 100)
+                {
+                    romanNumeral += "C";
+                    number -= 100;
+                }
+                else if (number >= 90)
+                {
+                    romanNumeral += "XC";
+                    number -= 90;
+                }
+                else if (number >= 50)
+                {
+                    romanNumeral += "L";
+                    number -= 50;
+                }
+                else if (number >= 40)
+                {
+                    romanNumeral += "XL";
+                    number -= 40;
+                }
+                else if (number >= 10)
                 {
                     romanNumeral += "X";
                     number -= 10;
                 }
-                else if (number >= 9)
+                else if (number == 9)
                 {
                     romanNumeral += "IX";
                     number -= 9;
